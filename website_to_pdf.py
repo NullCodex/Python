e0aca5087108a91515d1fcf57ec4a0492616ac1a
NullCodex <jamesonyu95@gmail.com>
2014-07-24 10:47:28 -0400

    Update and rename internet.py to website_to_pdf.py

--- website_to_pdf.py
+++ website_to_pdf.py
@@ -7,11 +7,11 @@ from PyQt4.QtWebKit import *
 
 app = QApplication(sys.argv)
 web = QWebView()
-web.load(QUrl("http://en.wikipedia.org/wiki/Main_Page"))
+web.load(QUrl("Insert website here"))
 printer = QPrinter()
 printer.setPageSize(QPrinter.A4)
 printer.setOutputFormat(QPrinter.PdfFormat)
-printer.setOutputFileName("News.pdf")
+printer.setOutputFileName("insert desired name.pdf")
 
 def convertIt():
     web.print_(printer)
